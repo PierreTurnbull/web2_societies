0512bd07997868670827624b61a0a29874dadc70
sl <sl@macbook-pro-de-toumi.home>
2018-12-16 14:05:43 +0100

    shader

--- src/components/atomes/Scene/shaders.js
+++ src/components/atomes/Scene/shaders.js
@@ -1,5 +1,7 @@
+const random = Math.random();
 const vertexShader = `
   varying vec2 vUv;
+  varying vec2 vUv1;
   uniform vec2 size;
   uniform vec2 resolution;
   uniform vec2 uvRate1;
@@ -7,10 +9,10 @@ const vertexShader = `
   void main() {
     vUv = uv;
     vec2 _uv = uv - 0.5;
-    vUv = _uv;
-    vUv *= uvRate1.xy;
+    vUv1 = _uv;
+    vUv1 *= uvRate1.xy;
 
-    vUv += 0.5;
+    vUv1 += 0.5;
 
     gl_Position =  projectionMatrix * modelViewMatrix * vec4(position, 1.0);
   }
@@ -18,12 +20,13 @@ const vertexShader = `
 
 const fragmentShader = `
   varying vec2 vUv;
+  varying vec2 vUv1;
   uniform float time;
   uniform sampler2D texture;
 
   void main() {
-    float distort = cos(vUv.y * abs(sin(time/5.)*3. + vUv.x * 2.) / abs(sin(.5)) + time/2.)*.1;
-    vec4 color = texture2D(texture, vec2(vUv.x + distort * .5,vUv.y + distort * 2.2));
+    float distort = cos(vUv.y * abs(sin(time/${random}/10.)*3. + vUv.x * 2.) / abs(sin(.5)) + time/2.)*.1;
+    vec4 color = texture2D(texture, vec2(vUv.x + distort * .5,vUv.y + distort * .2));
     gl_FragColor = vec4(vec3(color.r, color.g , color.b) * 1.0, 1.0);
   }
 `
